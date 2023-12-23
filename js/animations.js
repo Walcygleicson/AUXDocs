6e9c83f0c4c50e713bcac4c83d6bfb42ee935fae
Walcygleicson <walcygleicsonoliveira@gmail.com>
2023-12-23 19:01:58 -0300

    Footer Redesign

--- js/animations.js
+++ js/animations.js
@@ -117,4 +117,19 @@ const closeDetails = (close) => {
 }
 
 
+function isMobileDevice() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+        return true; // está utilizando celular
+    }
+    else {
+        return false; // não é celular
+    }
+}
 
